9f65d87fef6954c75cfc8fe39ba07980d5b28460
Logan <lthomas011796@gmail.com>
2025-04-09 13:17:29 -0400

    Updated Push

--- components/Popup.js
+++ components/Popup.js
@@ -1,20 +1,35 @@
 class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
-    this._popCloseBtn = this._popupElement.querySelector(".popup__close"); // ✅ fixed class name
+    this._popCloseBtn = this._popupElement.querySelector(".popup__close");
+    this._handleEscapeClose = this._handleEscapeClose.bind(this); // bind to keep context
+  }
+
+  _handleEscapeClose(evt) {
+    if (evt.key === "Escape") {
+      this.close(); // ✅ Call the close method
+    }
   }
 
   open() {
     this._popupElement.classList.add("popup_visible");
+    document.addEventListener("keyup", this._handleEscapeClose);
   }
 
   close() {
     this._popupElement.classList.remove("popup_visible");
+    document.removeEventListener("keyup", this._handleEscapeClose); // ✅ clean up
     console.log("close Method Called");
   }
 
   setEventListeners() {
     this._popCloseBtn.addEventListener("click", () => this.close());
+
+    this._popupElement.addEventListener("mousedown", (evt) => {
+      if (evt.target === this._popupElement) {
+        this.close();
+      }
+    });
   }
 }
 
